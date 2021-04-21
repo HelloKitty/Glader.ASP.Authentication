ea034638fa2591779ac9539b7f209253db622866
HelloKitty <andrew.blakely@ymail.com>
2021-04-21 13:07:03 -0500

    Added GladerASPAuthenticationConstants

---
+++ src/Glader.ASP.Authentication.Models/GladerASPAuthenticationConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.Authentication
+{
+	public static class GladerASPAuthenticationConstants
+	{
+		public const string SUBACCOUNT_ID_HEADER = "SAID";
+	}
+}
