8cd299708fbdc5f95609297f630ad245f8e9f6be
HelloKitty <andrew.blakely@ymail.com>
2021-04-21 13:52:14 -0500

    Added SubAccountHeaderParser

---
+++ src/Glader.ASP.Authentication.Common.Server/Services/SubAccountHeaderParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace Glader.ASP.Authentication
+{
+	public sealed class SubAccountHeaderParser
+	{
+		/// <summary>
+		/// Indicates if the request has a sub-account ID header.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		public bool HasHeader(HttpRequest request)
+		{
+			if (request == null) throw new ArgumentNullException(nameof(request));
+
+			return request.Headers.ContainsKey(GladerASPAuthenticationConstants.SUBACCOUNT_ID_HEADER);
+		}
+
+		/// <summary>
+		/// Parses the sub-account ID from the request.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		public int Parse(HttpRequest request)
+		{
+			if (request == null) throw new ArgumentNullException(nameof(request));
+
+			string header = request.Headers[GladerASPAuthenticationConstants.SUBACCOUNT_ID_HEADER];
+
+			return int.Parse(header);
+		}
+	}
+}
